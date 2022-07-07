857ba4ca2b549d5d3966b15c15fc8a162ba2361c
4o4E <869951226@qq.com>
2022-07-07 23:26:10 +0800

    添加点击限制的跳过权限, 完善配置文件中的注释, 添加打掉展示框时的处理

--- src/main/kotlin/listener/ClickListener.kt
+++ src/main/kotlin/listener/ClickListener.kt
@@ -3,7 +3,9 @@ package top.e404.boom.listener
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.hanging.HangingBreakByEntityEvent
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import top.e404.boom.PL
@@ -27,6 +29,77 @@ object ClickListener : EListener(PL) {
             if (!blockRegex.matches(block.type.name)
                 || !itemRegex.matches(item)
             ) continue
+            if (player.hasPermission("boom.bypass.block")) {
+                plugin.debug {
+                    Lang[
+                            "click.pass",
+                            "player" to player.name,
+                            "item" to item,
+                            "target" to block.type.name,
+                            "world" to player.world.name,
+                            "x" to block.x,
+                            "y" to block.y,
+                            "z" to block.z,
+                    ]
+                }
+                return
+            }
+            isCancelled = true
+            plugin.debug {
+                Lang[
+                        "click.prevent",
+                        "player" to player.name,
+                        "item" to item,
+                        "target" to block.type.name,
+                        "world" to player.world.name,
+                        "x" to block.x,
+                        "y" to block.y,
+                        "z" to block.z,
+                ]
+            }
+            return
+        }
+        plugin.debug {
+            Lang[
+                    "click.pass",
+                    "player" to player.name,
+                    "item" to item,
+                    "target" to block.type.name,
+                    "world" to player.world.name,
+                    "x" to block.x,
+                    "y" to block.y,
+                    "z" to block.z,
+            ]
+        }
+    }
+
+    @EventHandler
+    fun BlockBreakEvent.onEvent() {
+        val block = block
+        val cfg = Config.getEachOrGlobal(player.world.name) { preventClickBlock } ?: return
+        val item = player.inventory.itemInMainHand.type.name
+        for ((itemRegex, blockRegex, type) in cfg) {
+            // 忽略交互检测
+            if (type == Config.ClickType.RIGHT) continue
+            // 物品或方块不匹配
+            if (!blockRegex.matches(block.type.name)
+                || !itemRegex.matches(item)
+            ) continue
+            if (player.hasPermission("boom.bypass.block")) {
+                plugin.debug {
+                    Lang[
+                            "click.pass",
+                            "player" to player.name,
+                            "item" to item,
+                            "target" to block.type.name,
+                            "world" to player.world.name,
+                            "x" to block.x,
+                            "y" to block.y,
+                            "z" to block.z,
+                    ]
+                }
+                return
+            }
             isCancelled = true
             plugin.debug {
                 Lang[
@@ -68,6 +141,22 @@ object ClickListener : EListener(PL) {
             if (!entityRegex.matches(entity)
                 || !itemRegex.matches(item)
             ) continue
+            if (player.hasPermission("boom.bypass.entity")) {
+                plugin.debug {
+                    val l = rightClicked.location
+                    Lang[
+                            "click.pass",
+                            "player" to player.name,
+                            "item" to item,
+                            "target" to entity,
+                            "world" to player.world.name,
+                            "x" to l.blockX,
+                            "y" to l.blockY,
+                            "z" to l.blockZ,
+                    ]
+                }
+                return
+            }
             isCancelled = true
             plugin.debug {
                 val l = rightClicked.location
@@ -100,6 +189,67 @@ object ClickListener : EListener(PL) {
     }
 
     @EventHandler
+    fun HangingBreakByEntityEvent.onEvent() {
+        val remover = remover
+        if (remover !is Player) return
+        val entity = entity
+        val cfg = Config.getEachOrGlobal(remover.world.name) { preventClickEntity } ?: return
+        val item = remover.inventory.itemInMainHand.type.name
+        for ((itemRegex, entityRegex, type) in cfg) {
+            // 忽略交互检测
+            if (type == Config.ClickType.RIGHT) continue
+            // 物品或实体不匹配
+            if (!entityRegex.matches(entity.type.name)
+                || !itemRegex.matches(item)
+            ) continue
+            if (remover.hasPermission("boom.bypass.entity")) {
+                plugin.debug {
+                    val l = entity.location
+                    Lang[
+                            "click.pass",
+                            "player" to remover.name,
+                            "item" to item,
+                            "target" to entity.type.name,
+                            "world" to remover.world.name,
+                            "x" to l.x,
+                            "y" to l.y,
+                            "z" to l.z,
+                    ]
+                }
+                return
+            }
+            isCancelled = true
+            plugin.debug {
+                val l = entity.location
+                Lang[
+                        "click.prevent",
+                        "player" to remover.name,
+                        "item" to item,
+                        "target" to entity.type.name,
+                        "world" to remover.world.name,
+                        "x" to l.x,
+                        "y" to l.y,
+                        "z" to l.z,
+                ]
+            }
+            return
+        }
+        plugin.debug {
+            val l = entity.location
+            Lang[
+                    "click.pass",
+                    "player" to remover.name,
+                    "item" to item,
+                    "target" to entity.type.name,
+                    "world" to remover.world.name,
+                    "x" to l.x,
+                    "y" to l.y,
+                    "z" to l.z,
+            ]
+        }
+    }
+
+    @EventHandler
     fun EntityDamageByEntityEvent.onEvent() {
         val damager = damager
         if (damager !is Player) return
@@ -112,6 +262,22 @@ object ClickListener : EListener(PL) {
             if (!entityRegex.matches(entityType.name)
                 || !itemRegex.matches(item)
             ) continue
+            if (damager.hasPermission("boom.bypass.entity")) {
+                plugin.debug {
+                    val l = entity.location
+                    Lang[
+                            "click.pass",
+                            "player" to damager.name,
+                            "item" to item,
+                            "target" to entity.type.name,
+                            "world" to damager.world.name,
+                            "x" to l.x,
+                            "y" to l.y,
+                            "z" to l.z,
+                    ]
+                }
+                return
+            }
             isCancelled = true
             plugin.debug {
                 val l = entity.location
