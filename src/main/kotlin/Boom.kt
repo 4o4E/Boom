7796cd9e3f084676a2b55e31aa07f3666060af1d
4o4E <869951226@qq.com>
2022-07-11 22:24:48 +0800

    添加阻止玩家收到伤害的选项

--- src/main/kotlin/Boom.kt
+++ src/main/kotlin/Boom.kt
@@ -44,6 +44,7 @@ class Boom : EPlugin() {
         StickListener.register()
         DeathListener.register()
         ClickListener.register()
+        DamageListener.register()
         Update.init()
         for (line in logo) info(line)
         info("&a加载完成, 作者404E, 感谢使用")
---
+++ src/main/kotlin/listener/DamageListener.kt
@@ -0,0 +1,39 @@
+package top.e404.boom.listener
+
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageEvent
+import top.e404.boom.PL
+import top.e404.boom.config.Config
+import top.e404.eplugin.listener.EListener
+
+object DamageListener : EListener(PL) {
+    // 伤害取消
+    @EventHandler
+    fun EntityDamageEvent.onEvent() {
+        val entity = entity
+        if (entity !is Player) return
+        val world = entity.world.name
+        val location = entity.location
+        if (Config.getEachOrGlobal(world) { preventPlayerDamage } == true) {
+            isCancelled = true
+            plugin.debug(
+                "player_damage.prevent",
+                "player" to entity.name,
+                "world" to world,
+                "x" to location.blockX,
+                "y" to location.blockY,
+                "z" to location.blockZ,
+            )
+            return
+        }
+        plugin.debug(
+            "player_damage.pass",
+            "player" to entity.name,
+            "world" to world,
+            "x" to location.blockX,
+            "y" to location.blockY,
+            "z" to location.blockZ,
+        )
+    }
+}
