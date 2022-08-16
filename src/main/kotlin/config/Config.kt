60a12015c5ec3381468814a0006b4ae424be8410
4o4E <869951226@qq.com>
2022-08-16 14:42:57 +0800

    添加注释

--- src/main/kotlin/config/Config.kt
+++ src/main/kotlin/config/Config.kt
@@ -43,6 +43,16 @@ object Config : ERegionConfig<BoomConfig, RgConfig>(
         get() = config.stick.lore.map { it.color() }
 }
 
+/**
+ * 代表一个设置的实现类
+ *
+ * @property debug 是否启用控制台debug
+ * @property update 是否检查更新
+ * @property global 全局设置
+ * @property region 区域设置
+ * @property each 世界设置
+ * @property stick 调试棒设置
+ */
 @Serializable
 data class RgConfig(
     var debug: Boolean = false,
@@ -53,6 +63,30 @@ data class RgConfig(
     val stick: StickConfig,
 ) : RegionConfig<BoomConfig>
 
+/**
+ * 代表一个具体的设置
+ *
+ * @property explosion 实体爆炸设置
+ * @property disableFireSpread 阻止火焰蔓延
+ * @property disableFireBurn 阻止火焰烧毁方块
+ * @property protectFarmland 保护耕地方块不被踩坏
+ * @property protectVillagerToWitch 阻止村民变成女巫
+ * @property protectVillagerToZombie 阻止村民变成僵尸村民
+ * @property protectZombieVillagerToVillager 阻止僵尸村民变成村民
+ * @property protectZombieToDrowned 阻止僵尸变成溺尸
+ * @property preventEndermanPickup 阻止末影人搬起方块
+ * @property fixArmorstandPose 在盔甲架生成的时候摆正并设置为有双手
+ * @property keepInventoryOnDeath 玩家死亡时保留背包
+ * @property keepLevelOnDeath 玩家死亡时保留经验
+ * @property preventPlayerDamage 阻止玩家受到伤害
+ * @property preventUseBed 阻止玩家使用床
+ * @property preventUseRespawnAnchor 阻止玩家使用重生锚
+ * @property preventUseCommand 阻止玩家使用指令
+ * @property transformUseCommand 指令转接
+ * @property limitEntitySpawn 限制实体生成
+ * @property preventClickBlock 阻止点击方块
+ * @property preventClickEntity 阻止点击实体
+ */
 @Serializable
 data class BoomConfig(
     val explosion: Map<String, ExplosionConfig>? = null,
@@ -96,12 +130,25 @@ data class BoomConfig(
     val preventClickEntity: List<ClickEntityConfig>? = null,
 )
 
+/**
+ * 阻止实体爆炸的设置
+ *
+ * @property enable 是否启用
+ * @property cancel 是否直接取消
+ */
 @Serializable
 data class ExplosionConfig(
     val enable: Boolean = true,
     val cancel: Boolean = false
 )
 
+/**
+ * 阻止玩家使用的设置
+ *
+ * @property enable 是否启用
+ * @property message 阻止时给玩家发送的消息
+ * @property sound 阻止时给玩家播放的音效
+ */
 @Serializable
 data class PreventUseConfig(
     val enable: Boolean,
@@ -109,6 +156,14 @@ data class PreventUseConfig(
     val sound: SoundConfig?
 )
 
+/**
+ * 阻止玩家使用指令的设置
+ *
+ * @property enable 是否启用
+ * @property message 消息设置
+ * @property log 日志设置
+ * @property regexes 正则列表
+ */
 @Serializable
 data class PreventUseCommand(
     val enable: Boolean,
@@ -117,12 +172,25 @@ data class PreventUseCommand(
     val regexes: List<@Serializable(RegexSerialization::class) Regex>
 )
 
+/**
+ * 消息设置
+ *
+ * @property user 给使用指令的玩家发送的消息
+ * @property console 控制台的消息
+ * @property op 给在线op发送的消息
+ */
 @Serializable
 data class Message(
     val user: String,
     val console: String,
     val op: String,
 ) {
+    /**
+     * 玩家执行阻止的指令时发送消息
+     *
+     * @param player 玩家
+     * @param command 执行的指令
+     */
     fun send(player: Player, command: String) {
         fun String.p() = placeholder(
             "player" to player.name,
@@ -135,6 +203,14 @@ data class Message(
     }
 }
 
+/**
+ * 日志设置
+ *
+ * @property enable 是否启用
+ * @property datetimeFormat 日志时间戳格式
+ * @property logFormat 日志格式
+ * @property file 日志文件
+ */
 @Serializable
 data class Log(
     val enable: Boolean,
@@ -156,6 +232,12 @@ data class Log(
             "command" to command
         )
 
+    /**
+     * 玩家执行阻止的指令时记录日志
+     *
+     * @param player 玩家
+     * @param command 执行的指令
+     */
     fun log(player: Player, command: String) {
         if (enable) PL.runTaskAsync {
             synchronized(file) {
@@ -165,12 +247,26 @@ data class Log(
     }
 }
 
+/**
+ * 指令转接设置
+ *
+ * @property enable 是否启用
+ * @property list 转接指令列表
+ */
 @Serializable
 data class TransformUseCommandConfig(
     val enable: Boolean,
     val list: List<CommandTrigger>
 )
 
+/**
+ * 转接指令
+ *
+ * @property regex 匹配正则
+ * @property trigger 触发的指令
+ * @property permission 触发所需指令
+ * @property noperm 无权限时返回的消息
+ */
 @Serializable
 data class CommandTrigger(
     @Serializable(RegexSerialization::class)
@@ -179,9 +275,21 @@ data class CommandTrigger(
     val permission: String? = null,
     val noperm: String = "",
 ) {
+    /**
+     * 检测正则是否匹配
+     *
+     * @param command 指令
+     */
     fun match(command: String) = regex.matches(command)
 }
 
+/**
+ * 点击方块触发设置
+ *
+ * @property item 物品id匹配正则
+ * @property block 方块id匹配正则
+ * @property type 检测类型
+ */
 @Serializable
 data class ClickBlockConfig(
     @Serializable(RegexSerialization::class)
@@ -192,6 +300,13 @@ data class ClickBlockConfig(
     val type: ClickType
 )
 
+/**
+ * 点击实体触发设置
+ *
+ * @property item 物品id匹配正则
+ * @property entity 实体id匹配正则
+ * @property type 检测类型
+ */
 @Serializable
 data class ClickEntityConfig(
     @Serializable(RegexSerialization::class)
@@ -202,6 +317,9 @@ data class ClickEntityConfig(
     val type: ClickType
 )
 
+/**
+ * [ClickType]的序列化和反序列化器
+ */
 object ClickTypeSerializer : KSerializer<ClickType> {
     override val descriptor =
         PrimitiveSerialDescriptor("top.e404.boom.config.ClickType.Companion.getSerializer", PrimitiveKind.STRING)
@@ -216,6 +334,11 @@ object ClickTypeSerializer : KSerializer<ClickType> {
     }
 }
 
+/**
+ * 点击类型
+ *
+ * @param regex 正则
+ */
 @Serializable
 enum class ClickType(
     regex: String
@@ -227,6 +350,12 @@ enum class ClickType(
     val regex = Regex(regex)
 }
 
+/**
+ * 调试棒设置
+ *
+ * @property name 调试棒名字
+ * @property lore 调试棒lore
+ */
 @Serializable
 data class StickConfig(
     val name: String,
